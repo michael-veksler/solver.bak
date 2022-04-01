e249af5ddc1365ca75b66a09eeebd2ae5a7c71f1
Michael Veksler <mickey.veksler@gmail.com>
2022-04-01 23:41:56 +0300

    Implemented exhaustive-search solver

---
+++ src/exhaustive_solver.hpp
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "binary_clause.hpp"
+#include "constraint.hpp"
+#include "utilities.hpp"
+#include <algorithm>
+#include <cinttypes>
+#include <cstdlib>
+#include <iterator>
+#include <ranges>
+#include <vector>
+
+namespace solver {
+
+template<typename Domain, std::integral VariableIndexType> class uniform_constraint_state
+{
+  public:
+    using domain_type = Domain;
+    using value_type = typename domain_type::value_type;
+    using param_index_t = VariableIndexType;
+    struct parameter_t
+    {
+        VariableIndexType variable_index;
+    };
+    explicit uniform_constraint_state(unsigned variables, const domain_type &domain) : m_variables(variables, domain) {}
+    const domain_type &get_domain(parameter_t param) const { return m_variables[param.variable_index]; }
+    value_type get_value(parameter_t param) const
+    {
+        const domain_type &domain = get_domain(param);
+        assert(domain.size() == 1);
+        return *domain.begin();
+    }
+    void set_domain(parameter_t param, const domain_type &dom) { m_variables[param.variable_index] = dom; }
+    void set_value(parameter_t param, value_type value)
+    {
+        m_variables[param.variable_index].clear();
+        m_variables[param.variable_index].insert(value);
+    }
+    state_saver<domain_type> saved_state(unsigned variable)
+    {
+        return state_saver<domain_type>{ m_variables[variable] };
+    }
+    size_t size() const { return m_variables.size(); }
+
+  private:
+    std::vector<domain_type> m_variables;
+};
+
+template<constraint_state constraint_state_t> class exhaustive_solver
+{
+  public:
+    using domain_type = typename constraint_state_t::domain_type;
+    using parameter_t = typename constraint_state_t::parameter_t;
+    using value_type = typename domain_type::value_type;
+
+    exhaustive_solver(unsigned variables, const domain_type &domain) : m_state(variables, domain) {}
+
+    bool solve() { return try_assignments(0); }
+
+    void add_clause(const std::vector<int> &literals)
+    {
+        std::vector<parameter_t> parameters;
+        parameters.reserve(literals.size());
+        std::transform(literals.begin(), literals.end(), std::back_inserter(parameters), [](int literal) {
+            return parameter_t{ static_cast<value_type>(std::abs(literal) - 1) };
+        });
+
+        std::vector<bool> positive_literals;
+        positive_literals.reserve(literals.size());
+        std::transform(literals.begin(), literals.end(), std::back_inserter(positive_literals), [](int literal) {
+            return literal > 0;
+        });
+
+        m_clauses.emplace_back(binary_clause<constraint_state_t>(parameters, positive_literals));
+    }
+    value_type get_value(unsigned index) const { return m_state.get_value(parameter_t{ static_cast<uint8_t>(index) }); }
+    size_t num_variables() const { return m_state.size(); }
+
+  private:
+    bool try_assignments(uint8_t depth)
+    {
+        if (depth >= m_state.size()) {
+            return std::ranges::all_of(
+                m_clauses, [this](const auto &constraint) { return constraint.is_satisfied(m_state); });
+        }
+
+        state_saver<domain_type> saved_domain = m_state.saved_state(depth);
+        for (value_type val : saved_domain.saved_state()) {
+            m_state.set_value(parameter_t{ depth }, val);
+            if (try_assignments(depth + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    uniform_constraint_state<domain_type, std::uint8_t> m_state;
+    std::vector<binary_clause<constraint_state_t>> m_clauses;
+};
+}// namespace solver
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#include <filesystem>
+#include <fstream>
 #include <functional>
 #include <iostream>
+#include <set>
 
+#include "dimacs_parser.hpp"
+#include "exhaustive_solver.hpp"
 #include <docopt/docopt.h>
 #include <spdlog/spdlog.h>
 
@@ -10,21 +15,18 @@
 #include <internal_use_only/config.hpp>
 
 static constexpr auto USAGE =
-    R"(Naval Fate.
+    R"(Solve problem
 
     Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
-Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
+          solve --exhaustive --dimacs=FILE
+          solve (-h | --help)
+          solve --version
+    Options:
+          -h --help       Show this screen.
+          --version       Show version.
+          --exhaustive    Use exhaustive-search strategy, which traverses all
+                          dom_size ** num_variables search space.
+          --dimacs=fILE   DIMACS formatted CNF file.
 )";
 
 int main(int argc, const char **argv)
@@ -37,16 +39,44 @@ int main(int argc, const char **argv)
                 myproject::cmake::project_name,
                 myproject::cmake::project_version));// version string, acquired from config.hpp via CMake
 
-        for (auto const &arg : args) {
-            std::cout << arg.first << "=" << arg.second << '\n';
+
+        fmt::print("c solving {}\n", args.at("--dimacs").asString());
+        std::ifstream dimacs_stream{ args.at("--dimacs").asString() };
+        if (!dimacs_stream) {
+            fmt::print("c Could not open file {}\n",
+                args.at("--dimacs").asString(),
+                std::filesystem::current_path().string());
+            fmt::print("s UNKNOWN\n");
+            return 1;
         }
-
-
-        // Use the default logger (stdout, multi-threaded, colored)
-        spdlog::info("Hello, {}!", "World");
-
-        fmt::print("Hello, from {}\n", "{fmt}");
+        using domain_t = std::set<bool>;
+        domain_t unset = { false, true };
+        using solver_t = solver::exhaustive_solver<solver::uniform_constraint_state<domain_t, std::uint8_t>>;
+        std::unique_ptr<solver_t> solver_ptr;
+        auto constructor = [&](unsigned variables, unsigned constraints) {
+            std::ignore = constraints;
+            solver_ptr = std::make_unique<solver_t>(variables, unset);
+        };
+        auto add_clause = [&](const std::vector<int> &literals) { solver_ptr->add_clause(literals); };
+        solver::parse_dimacs(dimacs_stream, constructor, add_clause);
+        if (!solver_ptr) {
+            fmt::print("s UNKNOWN\n");
+            return 1;
+        }
+        if (solver_ptr->solve()) {
+            fmt::print("s SATISFIABLE\n");
+            fmt::print("v ");
+            for (unsigned i = 0; i != solver_ptr->num_variables(); ++i) {
+                if (solver_ptr->get_value(i))
+                    fmt::print("{} ", i + 1);
+                else
+                    fmt::print("-{} ", i + 1);
+            }
+            fmt::print("0\n");
+        } else {
+            fmt::print("s UNSATISFIABLE\n");
+        }
     } catch (const std::exception &e) {
-        fmt::print("Unhandled exception in main: {}", e.what());
+        fmt::print("Unhandled exception in main: {}\n", e.what());
     }
 }
