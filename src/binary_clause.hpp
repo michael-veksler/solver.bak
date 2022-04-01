f2d411c506d9f55f586a7092a90c08069c5bf1bc
Michael Veksler <mickey.veksler@gmail.com>
2022-04-01 13:49:53 +0300

    Split constraint concepts into basic/propagating
    If the state supports propagating concept, then
    binary_clause implements propagate() otherwise
    it does not.

--- src/binary_clause.hpp
+++ src/binary_clause.hpp
@@ -9,14 +9,14 @@
 namespace solver {
 template<constraint_state state_template_t>
 requires std::is_same_v<typename state_template_t::domain_type::value_type, bool>
-class binary_clause
+class binary_clause_base
 {
   public:
     using state_t = state_template_t;
     using param_index_t = typename state_t::param_index_t;
     using parameter_t = typename state_t::parameter_t;
 
-    explicit binary_clause(const std::vector<parameter_t> &parameters, const std::vector<bool> &positive_literals)
+    explicit binary_clause_base(const std::vector<parameter_t> &parameters, const std::vector<bool> &positive_literals)
     {
         assert(positive_literals.size() == parameters.size());
         m_literals.reserve(positive_literals.size());
@@ -33,6 +33,43 @@ class binary_clause
         }
         return false;
     }
+    parameter_t get_parameter(unsigned index) const { return m_literals[index].first; }
+    bool is_positive_literal(unsigned index) const { return m_literals[index].second; }
+    size_t num_parameters() const { return m_literals.size(); }
+
+  private:
+    std::vector<std::pair<parameter_t, bool>> m_literals;
+};
+
+template<constraint_state state_template_t>
+requires bool_domain<state_template_t>
+class binary_clause : public binary_clause_base<state_template_t>
+{
+  public:
+    using state_t = state_template_t;
+    using param_index_t = typename state_t::param_index_t;
+    using parameter_t = typename state_t::parameter_t;
+
+    explicit binary_clause(const std::vector<parameter_t> &parameters, const std::vector<bool> &positive_literals)
+        : binary_clause_base<state_template_t>(parameters, positive_literals)
+    {}
+};
+
+template<propagating_constraint_state state_template_t>
+requires bool_domain<state_template_t>
+class binary_clause<state_template_t> : public binary_clause_base<state_template_t>
+{
+  public:
+    using state_t = state_template_t;
+    using param_index_t = typename state_t::param_index_t;
+    using parameter_t = typename state_t::parameter_t;
+    using binary_clause_base<state_template_t>::num_parameters;
+    using binary_clause_base<state_template_t>::get_parameter;
+    using binary_clause_base<state_template_t>::is_positive_literal;
+
+    explicit binary_clause(const std::vector<parameter_t> &parameters, const std::vector<bool> &positive_literals)
+        : binary_clause_base<state_template_t>(parameters, positive_literals)
+    {}
     propagation_result_t propagate(state_t &state, unsigned trigger_param)
     {
         if (trigger_param == m_watch0) {
@@ -41,7 +78,7 @@ class binary_clause
         if constexpr (state_t::only_watches_trigger) {
             return propagate_by_watch(state, m_watch1, m_watch0);
         } else {
-            if (trigger_param >= m_literals.size() || trigger_param > std::numeric_limits<param_index_t>::max()) {
+            if (trigger_param >= num_parameters() || trigger_param > std::numeric_limits<param_index_t>::max()) {
                 throw std::out_of_range("tigger param is too big");
             }
             m_watch1 = static_cast<param_index_t>(trigger_param);
@@ -51,28 +88,33 @@ class binary_clause
 
 
   private:
-    propagation_result_t propagate_by_watch(state_t &state, param_index_t &triggered_watch, param_index_t other_watch)
+    propagation_result_t
+        propagate_by_watch(state_t &state, param_index_t &triggered_watch, const param_index_t &other_watch) const
     {
-        if (state.get_value(m_literals[triggered_watch].first) == m_literals[triggered_watch].second) {
+        if (state.get_value(get_parameter(triggered_watch)) == is_positive_literal(triggered_watch)) {
             return propagation_result_t::SAT;
         }
         param_index_t next_watch = get_next_watch(triggered_watch);
         for (; next_watch != triggered_watch; next_watch = get_next_watch(next_watch)) {
-            auto [param, positive_literal] = m_literals[next_watch];
+            parameter_t param = get_parameter(next_watch);
+            bool positive_literal = is_positive_literal(next_watch);
             if (next_watch != other_watch && state.get_domain(param).count(positive_literal)) {
                 if (state.get_domain(param).size() == 1) {
                     return propagation_result_t::SAT;
                 }
 
-                state.unregister_watch(m_literals[triggered_watch].first);
+                state.unregister_watch(get_parameter(triggered_watch));
                 triggered_watch = next_watch;
                 state.register_watch(param);
                 return propagation_result_t::CONSISTENT;
             }
         }
-        if (const auto &literal = m_literals[other_watch]; state.get_domain(literal.first).count(literal.second)) {
-            if (state.get_domain(literal.first).size() > 1) {
-                state.set_value(literal.first, literal.second);
+        parameter_t other_param = get_parameter(other_watch);
+        bool other_is_positive = is_positive_literal(other_watch);
+
+        if (state.get_domain(other_param).count(other_is_positive)) {
+            if (state.get_domain(other_param).size() > 1) {
+                state.set_value(other_param, other_is_positive);
             }
             return propagation_result_t::SAT;
         }
@@ -80,10 +122,9 @@ class binary_clause
     }
     param_index_t get_next_watch(const param_index_t &watch) const
     {
-        return std::cmp_equal(watch + 1, m_literals.size()) ? 0 : watch + 1;
+        return std::cmp_equal(watch + 1, num_parameters()) ? 0 : watch + 1;
     }
 
-    std::vector<std::pair<parameter_t, bool>> m_literals;
     param_index_t m_watch0 = 0;
     param_index_t m_watch1 = 1;
 };
--- src/constraint.hpp
+++ src/constraint.hpp
@@ -8,17 +8,10 @@ namespace solver {
 template<typename T>
 concept constraint_state = requires(T a)
 {
-    // {
-    //     std::as_const(T::only_watches_trigger)
-    //     } -> std::same_as<const bool &>;
     typename T::param_index_t;
     typename T::parameter_t;
     typename T::domain_type;
     typename T::domain_type::value_type;
-    T::only_watches_trigger;
-    { a.register_watch(std::declval<typename T::parameter_t>()) };
-    { a.unregister_watch(std::declval<typename T::parameter_t>()) };
-    //{ T::only_watches_trigger } -> std::same_as<const bool>;
     std::integral<typename T::param_index_t>;
     {
         a.get_value(std::declval<typename T::parameter_t>())
@@ -29,6 +22,18 @@ concept constraint_state = requires(T a)
     { a.set_value(std::declval<typename T::parameter_t>(), std::declval<typename T::domain_type::value_type>()) };
     { a.set_domain(std::declval<typename T::parameter_t>(), std::declval<typename T::domain_type>()) };
 };
+
+template<typename T>
+concept propagating_constraint_state = constraint_state<T> && requires(T a)
+{
+    T::only_watches_trigger;
+    { a.register_watch(std::declval<typename T::parameter_t>()) };
+    { a.unregister_watch(std::declval<typename T::parameter_t>()) };
+};
+
+template<typename T>
+concept bool_domain = std::is_same_v<typename T::domain_type::value_type, bool>;
+
 
 enum class propagation_result_t : int8_t { UNSAT, CONSISTENT, SAT };
 
